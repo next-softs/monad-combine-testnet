daeca2ceef5493c0914cf0aaf3129f145f6c4b56
next-softs <67592943+next-softs@users.noreply.github.com>
2025-03-24 17:08:56 +0700

    Update config.py

--- config.py
+++ config.py
@@ -109,7 +109,7 @@ class SwapsSettings:
     delay_swap = [15, 60]
 
     # задержка между сессиями
-    delay_sessions = [3600*12, 3600*36]
+    delay_sessions = [3600, 3600]
 
 class StakeSettings:
     # какие используем сервисы для стейкинга ["apriori", "kintsu", "magma", "shmonad"]
@@ -144,7 +144,7 @@ class RandomSettings:
     delay_actions = [60, 600]
 
     # задержка между сессиями [от, до]
-    delay_sessions = [3600*8, 3600*24]
+    delay_sessions = [3600, 3600]
 
     # использовать кран ["gas.zip"]
     faucets = ["gas.zip"]
